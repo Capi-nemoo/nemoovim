7ff3d59cfc4fb1ea53f5729cb455ce2e4a45404a
capi-nemoo <capi_nemoo@protonmail.com>
2024-11-11 06:39:08 -0600

    tres teribles scripts xd

--- lua/user/ui/dashboard.lua
+++ lua/user/ui/dashboard.lua
@@ -2,8 +2,6 @@ local alpha = require('alpha')
 local dashboard = require('alpha.themes.dashboard')
 local Path = require('plenary.path')
 
-
-
 dashboard.section.header.val = {
     [[███╗   ██╗███████╗███╗   ███╗ ██████╗  ██████╗   ██╗   ██╗██╗███╗   ███╗]],
     [[████╗  ██║██╔════╝████╗ ████║██╔═══██╗██╔═══██╗  ██║   ██║██║████╗ ████║]],
@@ -15,13 +13,17 @@ dashboard.section.header.val = {
 
 
 
+
 -- Set menu
 -- You can customize the menu to include commands you use frequently
 dashboard.section.buttons.val = {
     dashboard.button("e", "  New file" , "<cmd>ene <BAR> startinsert <CR>"),
-    dashboard.button("f", "󰮗  Find file", "<cmd>Telescope find_files<CR>"),
+    dashboard.button("f", "󰮗  Find file", "<cmd>Telescope <CR>"),
     dashboard.button("r", "  Recent files", "<cmd>Telescope oldfiles<CR>"),
-    dashboard.button("s", "⚙️  Settings", "<cmd>e $MYVIMRC<CR>"),
+    dashboard.button("p", "  Projects", "<cmd>lua require('telescope.builtin').find_files({ cwd = '~/repos', prompt_title = 'Projects', find_command = { 'find', '.', '-mindepth', '1', '-maxdepth', '1', '-type', 'd' } })<CR>"),
+    dashboard.button("p", "📁  Git Repositories", "<cmd>lua require('telescope.builtin').find_files({ cwd = '~/repos', prompt_title = 'Git Repositories', find_command = { 'find', '.', '-mindepth', '1', '-maxdepth', '1', '-type', 'd', '-exec', 'test', '-e', '{}/.git', ';', '-print' } })<CR>"),
+    dashboard.button("p", "📁  Git Repositories (Global)", "<cmd>lua require('telescope.builtin').find_files({ prompt_title = 'Git Repositories', find_command = { 'find', '/', '-type', 'd', '-name', '.git', '-prune', '-exec', 'dirname', '{}', ';' } })<CR>"),
+    dashboard.button("s", "⚙️  Settings", "<cmd>Telescope find_files<CR>"),
     dashboard.button("u", "  Update plugins", "<cmd>PackerSync<CR>"),
     dashboard.button("q", "󰩈  Quit", "<cmd>qa<CR>"),
 }
@@ -31,3 +33,5 @@ dashboard.section.buttons.val = {
 dashboard.section.footer.val = {
     "el nemo cmamo",
 }
+
+alpha.setup(dashboard.config)
