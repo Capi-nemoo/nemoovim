dee79456bc0ade4250c24124c6616872f15d4071
capi-nemoo <capi_nemoo@protonmail.com>
2025-01-29 15:26:18 -0600

    config organized :)

--- lua/nemoo/core/plugins.lua
+++ lua/nemoo/core/plugins.lua
@@ -10,11 +10,34 @@
 ----------------------------------------------------------------------------------------------
 
 return {
+ --                                 | |
+ --   __ _  ___ _ __   ___ _ __ __ _| |
+ --  / _` |/ _ \ '_ \ / _ \ '__/ _` | |
+ -- | (_| |  __/ | | |  __/ | | (_| | |
+ --  \__, |\___|_| |_|\___|_|  \__,_|_|
+ --   __/ |                            
+ --  |___/                             
+ --
+ --
   -- nvim-autopairs: Autopairs brackets
   { "windwp/nvim-autopairs", event = "InsertEnter", config = true },
 
   -- Comment-nvim: Quick commenting
   { "numToStr/Comment.nvim", event = "VeryLazy"},
+
+  -- toggleterm.nvim: Toggleable terminal
+  { "akinsho/toggleterm.nvim", version = "*", cmd = { "ToggleTerm", "TermExec" }, config = true },
+
+
+
+ --        _ _   
+ --       (_) |  
+ --   __ _ _| |_ 
+ --  / _` | | __|
+ -- | (_| | | |_ 
+ --  \__, |_|\__|
+ --   __/ |      
+ --  |___/       
 
   -- vim-fugitive: Git integration
   { "tpope/vim-fugitive", cmd = { "Git", "G" }, config = true },
@@ -25,8 +48,23 @@ return {
   -- diffview.nvim: Git diff viewer
   { "sindrets/diffview.nvim", cmd = "DiffviewOpen", dependencies = { "nvim-lua/plenary.nvim" }, config = true },
 
+ --  _                             _               _                                  _      _   _                 
+ -- | |                           | |             | |                                | |    | | (_)                
+ -- | |___ _ __     __ _ _ __   __| |   __ _ _   _| |_ ___   ___ ___  _ __ ___  _ __ | | ___| |_ _  ___  _ __  ___ 
+ -- | / __| '_ \   / _` | '_ \ / _` |  / _` | | | | __/ _ \ / __/ _ \| '_ ` _ \| '_ \| |/ _ \ __| |/ _ \| '_ \/ __|
+ -- | \__ \ |_) | | (_| | | | | (_| | | (_| | |_| | || (_) | (_| (_) | | | | | | |_) | |  __/ |_| | (_) | | | \__ \
+ -- |_|___/ .__/   \__,_|_| |_|\__,_|  \__,_|\__,_|\__\___/ \___\___/|_| |_| |_| .__/|_|\___|\__|_|\___/|_| |_|___/
+ --       | |                                                                  | |                                 
+ --       |_|                                                                  |_|                                 
+ --
+  -- nvim-lspconfig: LSP configurations
+  { "neovim/nvim-lspconfig", event = { "BufReadPre", "BufNewFile" }, config = true },
+
   -- nvim-cmp: Autocompletion
   { "hrsh7th/nvim-cmp", event = "InsertEnter", dependencies = { "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path", "L3MON4D3/LuaSnip" }, config = true },
+
+  -- mason.nvim: Manage LSP and tools
+  { "williamboman/mason.nvim", cmd = "Mason", config = true },
 
   -- lsp-inlayhints.nvim: LSP inlay hints
   { "lvimuser/lsp-inlayhints.nvim", event = "LspAttach", config = true },
@@ -34,17 +72,23 @@ return {
   -- lspkind.nvim: Icons for autocompletion
   { "onsails/lspkind.nvim", event = "VeryLazy", config = true },
 
-  -- nvim-lspconfig: LSP configurations
-  { "neovim/nvim-lspconfig", event = { "BufReadPre", "BufNewFile" }, config = true },
+  -- nvim-treesitter: Better syntax highlighting
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate", event = { "BufReadPost", "BufNewFile" }, config = true },
 
-  -- mason.nvim: Manage LSP and tools
-  { "williamboman/mason.nvim", cmd = "Mason", config = true },
+ --                    _             _   _             
+ --                   (_)           | | (_)            
+ --  _ __   __ ___   ___  __ _  __ _| |_ _  ___  _ __  
+ -- | '_ \ / _` \ \ / / |/ _` |/ _` | __| |/ _ \| '_ \ 
+ -- | | | | (_| |\ V /| | (_| | (_| | |_| | (_) | | | |
+ -- |_| |_|\__,_| \_/ |_|\__, |\__,_|\__|_|\___/|_| |_|
+ --                       __/ |                        
+ --                      |___/                         
+ --
+  -- telescope.nvim: Fuzzy finder
+  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" }, cmd = "Telescope", config = true },
 
   -- symbols-outline.nvim: Symbol outline
   { "simrat39/symbols-outline.nvim", cmd = "SymbolsOutline", config = true },
-
-  -- nvim-treesitter: Better syntax highlighting
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate", event = { "BufReadPost", "BufNewFile" }, config = true },
 
   -- aerial.nvim: Code outline window
   { "stevearc/aerial.nvim", cmd = { "AerialToggle", "AerialOpen" }, config = true },
@@ -52,8 +96,26 @@ return {
   -- neo-tree.nvim: Modern file explorer
   { "nvim-neo-tree/neo-tree.nvim", branch = "v3.x", dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim" }, cmd = "Neotree", config = true },
 
-  -- telescope.nvim: Fuzzy finder
-  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" }, cmd = "Telescope", config = true },
+ --  _    _ _____ 
+ -- | |  | |_   _|
+ -- | |  | | | |  
+ -- | |  | | | |  
+ -- | |__| |_| |_ 
+ --  \____/|_____|
+ --
+  -- lualine.nvim: Status line
+  { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" }, event = "VeryLazy", config = true },
+
+  -- rose-pine/neovim: Rose Pine colorscheme
+  { "rose-pine/neovim", name = "rose-pine", lazy = false, priority = 1000, config = true },
+
+ --  _              _     
+ -- | |            | |    
+ -- | |_ ___   ___ | |___ 
+ -- | __/ _ \ / _ \| / __|
+ -- | || (_) | (_) | \__ \
+ --  \__\___/ \___/|_|___/
+ --
 
   -- vim-be-good: Practice Vim motions
   { "ThePrimeagen/vim-be-good", cmd = "VimBeGood", config = true },
@@ -61,14 +123,4 @@ return {
   -- live-server.nvim: Live server for web
   { "barrett-ruth/live-server.nvim", cmd = "LiveServer", config = true },
 
-  -- toggleterm.nvim: Toggleable terminal
-  { "akinsho/toggleterm.nvim", version = "*", cmd = { "ToggleTerm", "TermExec" }, config = true },
-
-  -- lualine.nvim: Status line
-  { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" }, event = "VeryLazy", config = true },
-
-  -- rose-pine/neovim: Rose Pine colorscheme
-  { "rose-pine/neovim", name = "rose-pine", lazy = false, priority = 1000, config = true },
-
 }
-
