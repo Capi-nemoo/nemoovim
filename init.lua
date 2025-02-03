98b5c95e985276029cfc452be4895e70096cf659
capi-nemoo <capi_nemoo@protonmail.com>
2025-02-03 14:27:49 -0600

    uwu

--- init.lua
+++ init.lua
@@ -10,30 +10,3 @@ require("nemoo.core.options")  -- General settings
 require("nemoo.core.lazy")     -- Lazy bootstrap
 require("nemoo.core.autocmds") -- auto commands
 
--- Navigation
---
---require("nemoo.navigation.neo-tree")
---require("nemoo.navigation.harpoon") -- Any other specific plugin configurations
---require("nemoo.navigation.telescope") -- Any other specific plugin configurations
-
---- LSP
---require("nemoo.lsps.treesitter") -- Any other specific plugin configurations
---require("nemoo.lsps.lsp") -- Any other specific plugin configurations
---require("nemoo.lsps.completion") -- Any other specific plugin configurations
---require("nemoo.lsps.treesitter-textobjects")
---require("nemoo.lsps.mason")
---
----Utils
---require("nemoo.utils.live-server") --node server for web dev
---   --git
---require("nemoo.utils.git.gitsigns") -- flags on new or deleted lines
---require("nemoo.utils.git.fugitive") -- git wrapper for neovim 
---
---- UI
---require("nemoo.ui.rose-pine")   -- Colorscheme settings
---require("nemoo.ui.dashboard")       -- Dashboard setup
---require("nemoo.ui.colorscheme_picker")  --choose colorscheme from a curated list
---require("nemoo.ui.statusline")      -- Statusline configuration
---require("nemoo.ui.lualine") --line pretty 
---
-
